5a7d87c7cf652dae0e45fa7ffe47c292b2b0706a
bayumavindo <master.mavindo@gmail.com>
2025-11-03 08:40:22 +0700

    up

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f667dec1e083e3d00abac29259a41bba",
+const RESOURCES = {"flutter_bootstrap.js": "bfb2c666f192f9c2eb3109b08674e7cb",
 "version.json": "0876ea158fd26a4012502ed7d51aac51",
-"index.html": "0bae5821bb01a2f98e40d6ab760ecd7a",
-"/": "0bae5821bb01a2f98e40d6ab760ecd7a",
+"index.html": "edc5f3da48d308908e4c61938a97ee99",
+"/": "edc5f3da48d308908e4c61938a97ee99",
 "main.dart.js": "e06435fbb8a8b3466c5c371a34e02921",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "399a18b87a69c9b70a46745d160dbd4a",
